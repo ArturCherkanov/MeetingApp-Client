251211e4fdd50491f46799cd014a26e8708e47fb
Artur Cherkanov <artur.cherkanov@gmail.com>
2019-01-31 16:12:30 +0300

    Changed Code Aligment

--- src/Pages/Home/components/EventListItem/EventListItem.jsx
+++ src/Pages/Home/components/EventListItem/EventListItem.jsx
@@ -6,8 +6,10 @@ class EventListItem extends Component {
     render() {
         return (
             <li className={mainStyles.eventItem} data-value={this.props.event}>
-            <span>{this.props.event.time+" / "+this.props.event.message}</span></li>
+                <span>{this.props.event.time + " / " + this.props.event.message}</span>
+            </li>
         )
     }
 }
+
 export default EventListItem;
