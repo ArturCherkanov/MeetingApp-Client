47a0343c8046dd4e8ba80ac577d974f988daeb72
Artur Cherkanov <artur.cherkanov@gmail.com>
2019-01-31 15:41:51 +0300

    Added REST API

--- src/api/index.js
+++ src/api/index.js
@@ -1,31 +1,36 @@
 import axios from 'axios';
 
-import Paths from './paths'
+import { API_PATH, EVENTS_PATH, USERS_PATH } from './paths'
 
+// Users
 
-export const addUserToDB = (username, password) => {
-    axios.post('/api/create', {
+export const addUserToDB = (username, password) => (
+    axios.post(API_PATH + USERS_PATH, {
         username: username,
         password: password
-    });
-}
-
-export const putEvent = (message, time) => (
- axios.post('/api/events/putData', {
-        message: message,
-        time: time
     })
 )
 
-export const getEvents = () =>
-    axios.get("/api/events/get");
-
-export const findUserInDB = (username, password) => {
-    axios.get('/api/login', {
+export const findUserInDB = (username, password) => (
+    axios.get(API_PATH + USERS_PATH, {
         params: {
             username: username,
             password: password
         }
     })
-}
+)
+
+// Events
+
+export const putEvent = (message, time) => (
+    axios.post(API_PATH + EVENTS_PATH, {
+        message: message,
+        time: time
+    })
+)
+
+export const getEvents = () => (
+    axios.get(API_PATH + EVENTS_PATH)
+)
+
 
--- src/api/paths.js
+++ src/api/paths.js
@@ -1,4 +1,8 @@
-//  right paths will be added here
-export class Paths  {
-    baseURL = 'http://localhost:3001/';
-}
+    // API
+    export const API_PATH = '/api/'
+    
+    // Events
+    export const EVENTS_PATH = '/events/';
+    
+    // Users
+    export const USERS_PATH = '/users/'
