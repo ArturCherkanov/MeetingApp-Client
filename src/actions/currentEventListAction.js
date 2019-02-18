d6f9bdfba399a86e0202ccde535fa26a583fc039
Artur Cherkanov <artur.cherkanov@gmail.com>
2019-02-18 19:07:36 +0300

    Changed Navbar Structure

---
+++ src/actions/currentEventListAction.js
@@ -0,0 +1,10 @@
+import { getEvents } from '../api';
+
+export const getDataFromDb = (date) => dispatch => {
+    getEvents(date)
+        .then(res => res.data)
+        .then(currentEventList => dispatch({
+            type: 'SET_EVENTLIST',
+            payload: currentEventList,
+        }));
+};
