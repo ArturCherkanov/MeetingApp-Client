cbcf927e1520937afba345b886d83bfdd1468899
Artur Cherkanov <artur.cherkanov@gmail.com>
2019-03-25 17:31:01 +0300

    Changed addEventItem Action

--- src/actions/addEventItemAction.js
+++ src/actions/addEventItemAction.js
@@ -1,11 +1,11 @@
 import { getEvents, putEvent } from '../api/';
 
 
-export const addDataToDb = (message, time, isAsyncLoadEvents) => dispatch => {
+export const addDataToDb = (event, isAsyncLoadEvents) => dispatch => {
 
     dispatch({ type: 'ADD_EVENTITEM_REQUEST' });
 
-    putEvent(message, time, isAsyncLoadEvents)
+    putEvent(event, isAsyncLoadEvents)
         .then(res => res.data)
         .then(res => {
             // let event = {};
