70a7ba5856ba1de77c547cc0d6cc79f0aa693d78
Artur Cherkanov <artur.cherkanov@gmail.com>
2019-03-26 16:18:54 +0300

    Import New Redusers

--- src/reducers/rootReduser.jsx
+++ src/reducers/rootReduser.jsx
@@ -2,15 +2,22 @@ import { combineReducers } from 'redux';
 
 import modalView from './modalAppearance';
 import eventList from './eventsList';
-import refresh from './needRefresh';
+// import refresh from './needRefresh';
 import isToken from './isToken';
+import notification from './notidicationAppearance';
+import roomList from './roomsList';
+import Common from './Common';
+
+
 
 
 const rootReducer = combineReducers({
+    Common,
     modalView,
     eventList,
-    refresh,
     isToken,
+    notification,
+    roomList,
 });
 
 export default rootReducer;
