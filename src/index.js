4a443351c6d25683342306f3c6c946698bc8382d
Artur Cherkanov <artur.cherkanov@gmail.com>
2019-03-26 14:47:38 +0300

    Changed Order Of Import Components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import thunkMiddleware from 'redux-thunk';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers/rootReduser';
-import thunkMiddleware from 'redux-thunk';
-import { BrowserRouter } from 'react-router-dom';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
