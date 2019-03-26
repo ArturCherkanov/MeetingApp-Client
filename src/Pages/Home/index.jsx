80ec8ed45450ccb9c041e24e3cf822c8479380a2
Artur Cherkanov <artur.cherkanov@gmail.com>
2019-03-26 15:42:29 +0300

    Added Sockets  To Home Page

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import openSocket from 'socket.io-client';
 
 import MainContent from './components/MainContent/MainContent';
 
 export default class HomePage extends Component {
 
+    componentDidMount() {
+        const socket = openSocket('http://192.168.11.65:3001');
+        socket.on('event', (data) => {
+        });
+    }
+    // socket = io.connect('http://localhost:3001');
+    // var socket = io();
     render() {
         return (
             <MainContent />
